604e86db932246eefaa44c32d2b6ee141852b960
aanil8286-netizen <aanil8286@gmail.com>
2026-05-13 09:10:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('village-ride').then((cache) => cache.addAll(['index.html', 'driver.html']))
+const CACHE_NAME = 'village-ride-v2'; // ఇక్కడ పేరు మార్చాను
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // పాత దాన్ని వెంటనే తీసేస్తుంది
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      for (let name of names) caches.delete(name); // పాత క్యాచీ అంతా క్లీన్ చేస్తుంది
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request)); // డైరెక్ట్ నెట్ నుండి తీసుకుంటుంది
 });
